2f10c694f37db7c82acbb4235ccf0550315b2ea4
unknown <aotwbys@gmail.com>
2020-09-28 13:17:31 -0400

    re-routed

--- route.js
+++ route.js
@@ -5,11 +5,11 @@ var router = express.Router();
 router.get("/", function(req, res){
     console.log("We're at the basic view here.")
     //Render index view later
-    res.render("index");
+    res.render("home/index");
 });
 
 router.get("/home", function (req, res) {
-    res.render("home");
+    res.render("home/home");
 });
 
 //return the router object
