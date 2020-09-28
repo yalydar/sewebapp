ee779d44a642d722861dcd46a718696a173bb766
unknown <aotwbys@gmail.com>
2020-09-28 11:28:34 -0400

    routing with route.js communicating with app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var path = require("path");
 
+var routes = require("./routes");
+
 var app = express();
 
 app.set("port", process.env.PORT || 3000);
@@ -9,7 +11,7 @@ app.listen(app.get("port"), function(){
    console.log("Server started on port " + app.get("port")); 
 });
 
-// No Routing Here
+app.use(routes);
 
 
 
--- route.js
+++ route.js
@@ -3,9 +3,10 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function(req, res){
-    
     console.log("We're at the basic view here.")
     //Render index view later
     //res.render("index");
-
 });
+
+//return the router object
+module.exports = router; 
