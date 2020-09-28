fd3528dae5729d196fe1fa2ca0d0e71edbcc6616
unknown <aotwbys@gmail.com>
2020-09-28 11:26:48 -0400

    intermediate router

--- app.js
+++ app.js
@@ -8,3 +8,8 @@ app.set("port", process.env.PORT || 3000);
 app.listen(app.get("port"), function(){
    console.log("Server started on port " + app.get("port")); 
 });
+
+// No Routing Here
+
+
+
---
+++ route.js
@@ -0,0 +1,11 @@
+var express = require("express");
+
+var router = express.Router();
+
+router.get("/", function(req, res){
+    
+    console.log("We're at the basic view here.")
+    //Render index view later
+    //res.render("index");
+
+});
